e74b620702d336c655dd82b5bbeab5a8f8f36d15
theonlineaid <nkomorfaruk@gmail.com>
2024-10-08 18:54:43 +0600

    Add backup

--- src/hooks/useLayoutControl.ts
+++ src/hooks/useLayoutControl.ts
@@ -1,19 +1,11 @@
 import { useEffect, useState } from 'react';
 import { Layout } from 'react-grid-layout';
-// import { useDispatch, useSelector } from 'react-redux';
-// import { RootState } from '../redux/store';
 
 const useLayoutControl = () => {
   const [customLayout, setCustomLayout] = useState<Layout[]>([]);
   const [isEditingEnabled, setIsEditingEnabled] = useState(false);
-  // const dispatch = useDispatch();
-  // const { customLayout, isEditingEnabled } = useSelector((state: RootState) => state.layout);
-
-
-  // Retrieve layout from localStorage when component mounts
   const savedLayout = localStorage.getItem('layout');
 
-  // Initialize initialLayout based on savedLayout
   const initialLayout: Layout[] = savedLayout
     ? JSON.parse(savedLayout)
     : [
@@ -21,58 +13,8 @@ const useLayoutControl = () => {
         { i: 'b', x: 2, y: 0, w: 2, h: 2 },
         { i: 'c', x: 4, y: 0, w: 2, h: 2 },
         { i: 'd', x: 0, y: 0, w: 2, h: 2 },
-        // All side resize 
-        // { i: 'a', x: 0, y: 0, w: 4, h: 4,  resizeHandles: ['s', 'w', 'e', 'n', 'se', 'sw', 'ne', 'nw'] },
-        // { i: 'b', x: 2, y: 0, w: 2, h: 4,  resizeHandles: ['s', 'w', 'e', 'n', 'se', 'sw', 'ne', 'nw'] },
-        // { i: 'c', x: 4, y: 0, w: 2, h: 2,  resizeHandles: ['s', 'w', 'e', 'n', 'se', 'sw', 'ne', 'nw'] },
-        // { i: 'd', x: 0, y: 0, w: 2, h: 2,  resizeHandles: ['s', 'w', 'e', 'n', 'se', 'sw', 'ne', 'nw'] },
+      
       ];
-
-  // Initialize initialLayout based on savedLayout
-  // const initialLayout: Layout[] = savedLayout
-  //   ? JSON.parse(savedLayout)
-  //   : [
-  //     {
-  //       i: 'a',
-  //       x: 0,
-  //       y: 0,
-  //       w: 4,
-  //       h: 4,
-  //       resizeHandles: ['s', 'w', 'e', 'n', 'se', 'sw', 'ne', 'nw'],
-  //       isResizable: isEditingEnabled ? true : false,
-  //       isDraggable: isEditingEnabled ? true : false,
-  //     },
-  //     {
-  //       i: 'b',
-  //       x: 2,
-  //       y: 0,
-  //       w: 2,
-  //       h: 4,
-  //       resizeHandles: ['s', 'w', 'e', 'n', 'se', 'sw', 'ne', 'nw'],
-  //       isResizable: isEditingEnabled ? true : false,
-  //       isDraggable: isEditingEnabled ? true : false,
-  //     },
-  //     {
-  //       i: 'c',
-  //       x: 4,
-  //       y: 0,
-  //       w: 2,
-  //       h: 2,
-  //       resizeHandles: ['s', 'w', 'e', 'n', 'se', 'sw', 'ne', 'nw'],
-  //       isResizable: isEditingEnabled ? true : false,
-  //       isDraggable: isEditingEnabled ? true : false,
-  //     },
-  //     {
-  //       i: 'd',
-  //       x: 0,
-  //       y: 0,
-  //       w: 2,
-  //       h: 2,
-  //       resizeHandles: ['s', 'w', 'e', 'n', 'se', 'sw', 'ne', 'nw'],
-  //       isResizable: isEditingEnabled ? true : false,
-  //       isDraggable: isEditingEnabled ? true : false,
-  //     },
-  //   ];
 
   const handleLayoutChange = (newLayout: Layout[]) => {
     setCustomLayout(newLayout);
